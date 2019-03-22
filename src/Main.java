7c4a04864ec20febacade92acb69736b574c4d6c
Dmitry <rdi81@yandex.ru>
2019-03-22 23:04:55 +0300

    добавлено меню

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,45 @@
 import structure.ReadAllIssues;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
 
-        // список issues
-        List<String> listOfIssues = new ReadAllIssues().ReadAllIssuesFromFileToString();
 
-        //Массив для вывода
-        String[] issuesToString= new String[listOfIssues.size()];
-        issuesToString = listOfIssues.toArray(issuesToString);
+        System.out.println("Система отслеживания ошибок");
+        printMainText();
+
+        while (true) {
+            int input_numder = scanner.nextInt();
+            switch (input_numder) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    System.out.println("Проект - Пользователь - Запись об ошибке");
+                    //Вывод всех записей
+                    allIssues();
+                    break;
+                case 2:
+                    System.out.println("Список проектов");
+                    //поиск проектов
+                    break;
+                case 3:
+                    System.out.println("Список пользователе");
+                    //поиск пользователей
+
+                    break;
+                case 4:
+                    ReportMenu();
+                    break;
+            }
+            printMainText();
+
+        }
 
 
         // для десктопного приложения
@@ -26,7 +54,39 @@ public class Main {
 //            }
 //        });
 
+    }
 
+    public static void printMainText() {
+        System.out.print("Для отображения данных введите соответствующее число:\n" +
+                "--> все данные         - 1\n" +
+                "--> проекты            - 2\n" +
+                "--> пользователи       - 3\n" +
+                "--> сформировать отчет - 4\n" +
+                "--> выйти из программы - 0\n");
+    }
+
+
+    public static void allIssues() throws Exception {
+        // список issues
+        List<String> listOfIssues = new ReadAllIssues().ReadAllIssuesFromFileToString();
+
+        //Массив для вывода
+        String[] issuesToString = new String[listOfIssues.size()];
+        issuesToString = listOfIssues.toArray(issuesToString);
+    }
+
+
+    public static void ReportMenu() {
+        System.out.print("Введите название проекта (для возврата введите 0):  ");
+        scanner.nextLine();
+        String inputProject = scanner.nextLine();
+
+        System.out.print("Введите имя пользователя (для возврата введите 0):  ");
+        String inputUser = scanner.nextLine();
+
+        System.out.println("\"" + inputUser + "\" в проект \"" + inputProject + "\" добавил следующие записи:");
+
+        //поиск записей
     }
 
 
